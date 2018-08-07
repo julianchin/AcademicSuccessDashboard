b6b8d1b5f8d14ecf8f07dbd523261e89626777ed
liuyisi <yisi.liu17@gmail.com>
2018-08-07 13:50:27 -0500

    initial front-end part

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+advisors = [
+    {
+        'name': 'Corey Schafer',
+        'Department': 'Computer Science',
+        'grade': 'Senior',
+        'Email': 'xxx@illinois.edu',
+        'Philosophy':'Work hard, play harder!'
+    },
+    {
+        'name': 'John Smith',
+        'Department': 'Computer Science',
+        'grade': 'PhD',
+        'Email': 'xxx@illinois',
+        'Philosophy':'Just do it!'
+    }
+]
+
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/advisor")
+def advisor():
+    return render_template('advisor.html', advisors=advisors)
+
+@app.route("/wireless")
+def wireless():
+    return render_template('advisor.html', advisors=advisors)
+
+@app.route("/compass")
+def compass():
+    return render_template('advisor.html', advisors=advisors)
+
+@app.route("/mooddle")
+def moddle():
+    return render_template('advisor.html', advisors=advisors)
+
+if __name__ == '__main__':
+    app.run(debug=True)
